37b6748f9f7c6e28e98aa80360a962790bb58e6b
chaewon-huh <cw.huh@postech.ac.kr>
2025-02-17 13:21:11 +0900

    add : train code

---
+++ train_v0.0.1.sh
@@ -0,0 +1,25 @@
+
+accelerate launch train_flux_dlora_advanced.py \
+  --pretrained_model_name_or_path "$MODEL_NAME" \
+  --instance_data_dir "$INSTANCE_DIR" \
+  --output_dir "./schnell-1-v0.0.1" \
+  --mixed_precision "bf16" \
+  --resolution 768 \
+  --train_batch_size 1 \
+  --gradient_accumulation_steps 4 \
+  --optimizer "prodigy" \
+  --learning_rate 1.0 \
+  --lr_scheduler "constant" \
+  --lr_warmup_steps 0 \
+  --max_train_steps 2000 \
+  --gradient_checkpointing \
+  --token_abstraction "hcwf" \
+  --instance_prompt "a hcwf baby" \
+  --initializer_concept "korean baby" \
+  --train_transformer_frac 1 \
+  --train_text_encoder_ti \
+  --train_text_encoder_ti_frac 0.5 \
+  --lora_layers="attn.to_k,attn.to_q,attn.to_v,attn.to_out.0" \
+  --seed "0" \
+  --push_to_hub \
+  --resume_from_checkpoint="latest" \
